f8980740dd681ac6113790a700c9fbe7e36411b2
LeeY910 <luckyyadav04423@gmail.com>
2025-07-27 01:56:09 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,37 +1,3 @@
-// let todo = [];
-
-// let req = prompt("please enter your request");
-
-// while(true){
-//     if(req=="quit"){
-//         console.log("quitting app")
-//         break;
-//     }
-//     if (req=="list"){
-//         console.log("-----------");
-//         for(let i=0; i<todo.length; i++);{
-//             console.log(task);
-        
-//     }
-//     console.log("-----------");
-// } else if (req =="add"){
-//     let task = prompt("please enter your task you want to add");
-//     todo.push(task);
-//     console.log("task added");
-// } else if (req=="delete"){
-//     let indx = prompt("please enter the task index");
-//     todo.splice(indx ,  1);
-
-// }
-//   req= prompt("pls enter your request")
-
-
-
-// }
-
-
-
-
 
 let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
